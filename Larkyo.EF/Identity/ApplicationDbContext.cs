92e4116ef9dfaf3a3940231f22896e8658e4a0d3
Kieran Tsai <kierantsai@gmail.com>
2017-04-24 10:24:18 +1200

    Fixed DBContext inheritance.

--- Larkyo.EF/Identity/ApplicationDbContext.cs
+++ Larkyo.EF/Identity/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Larkyo.EF.Migrations;
 
 namespace Larkyo.EF.Identity
 {
-    class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(string connectionString, bool throwIfV1Schema)
+            :base(connectionString, throwIfV1Schema: throwIfV1Schema)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<LarkyoContext, Configuration>());
+        }
+
     }
 }
