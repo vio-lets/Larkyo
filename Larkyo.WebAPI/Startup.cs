04e48c0d6a86bdfedd2ec90e19ed1426277bafcf
Kieran Tsai <kierantsai@gmail.com>
2017-04-24 09:58:44 +1200

    Added Owin related packages and implemented basic startup.

---
+++ Larkyo.WebAPI/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Larkyo.WebAPI.Startup))]
+
+namespace Larkyo.WebAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            HttpConfiguration httpConfig = new HttpConfiguration();
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(httpConfig);
+        }
+    }
+}
