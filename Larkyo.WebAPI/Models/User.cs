6d18618e0537e6ea49e543da46186c05ef46740d
clyde Shen <clyde.shen@outlook.com>
2017-04-24 20:08:17 +1200

    CS - add user model

---
+++ Larkyo.WebAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Larkyo.WebAPI.Models
+{
+    public class User
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
