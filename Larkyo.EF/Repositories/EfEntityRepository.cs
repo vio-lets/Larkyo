f1e5a191b632fb764794f2490fd7ed1e17a2e12f
Kieran Tsai <kierantsai@gmail.com>
2017-08-01 07:14:09 +1200

    Started implementing application user repository.

---
+++ Larkyo.EF/Repositories/EfEntityRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Larkyo.Infrastructure.Repositories;
+
+namespace Larkyo.EF.Repositories
+{
+    public abstract class EfEntityRepository<T, TContext> : IRepository<T>
+        where T : class, new()
+        where TContext : DbContext, new()
+    {
+        public IEnumerable<T> FindAll()
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<T>().ToList();
+            }
+        }
+
+        public virtual T SingleOrDefault(Expression<Func<T, bool>> expression)
+        {
+            using (TContext context = new TContext())
+            {
+                return context.Set<T>().FirstOrDefault(expression);
+            }
+        }
+    }
+}
---
+++ Larkyo.Tests/UnitTests/Repositories/ApplicationUserRepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Larkyo.Core.Domain;
+using Larkyo.Domain;
+using Larkyo.Infrastructure.Repositories;
+using Larkyo.EF.Repositories;
+
+namespace Larkyo.Tests.UnitTests.Repositories
+{
+    [TestFixture]
+    class ApplicationUserRepositoryTest
+    {
+        [TestCase]
+        public void TestSingleOrDefault()
+        {
+            IRepository<ApplicationUser> repository = new ApplicationUserRepository();
+            ApplicationUser admin = repository.SingleOrDefault(u => u.UserName == "admin");
+
+            Assert.IsNotNull(admin);
+        }
+    }
+}
