2fb3b44dddcb76cda51f8a458c48a6925ffdc508
Kieran Tsai <kierantsai@gmail.com>
2017-08-03 07:46:21 +1200

    Implemented test setup and test teardown for testing the  application user repository.

--- Larkyo.Tests/UnitTests/Repositories/ApplicationUserRepositoryTest.cs
+++ Larkyo.Tests/UnitTests/Repositories/ApplicationUserRepositoryTest.cs
@@ -1,19 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using NUnit.Framework;
 using Larkyo.Core.Domain;
 using Larkyo.Domain;
+using Larkyo.Infrastructure.Data;
+using Larkyo.Infrastructure.Services;
 using Larkyo.Infrastructure.Repositories;
+using Larkyo.EF;
 using Larkyo.EF.Repositories;
+using Larkyo.EF.Services;
+
 
 namespace Larkyo.Tests.UnitTests.Repositories
 {
     [TestFixture]
     class ApplicationUserRepositoryTest
     {
+        [OneTimeSetUp]
+        public void Init()
+        {
+            Debug.Print("Init Test");
+            IUserService userService = new UserService();
+            IUser<string> result = userService.CreateUser("alfred", "test123");
+            IUser<string> user2 = userService.CreateUser("moana", "test123");
+            IUser<string> user3 = userService.CreateUser("wayne", "test123");
+            
+        }
+
         [TestCase]
         public void TestSingleOrDefault()
         {
@@ -21,6 +42,40 @@ namespace Larkyo.Tests.UnitTests.Repositories
             ApplicationUser admin = repository.SingleOrDefault(u => u.UserName == "admin");
 
             Assert.IsNotNull(admin);
+        }
+
+        [TestCase]
+        public void TestFindAll()
+        {
+            IRepository<ApplicationUser> repository = new ApplicationUserRepository();
+            IEnumerable<ApplicationUser> result = repository.FindAll();
+
+            Assert.AreEqual(4, result.Count());
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            ConnectionStringSettings css = ConfigurationManager.ConnectionStrings["DefaultConnection"];
+
+            if(css != null)
+            {
+                using (SqlConnection connection = new SqlConnection(css.ConnectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandType = CommandType.Text;
+
+                    cmd.CommandText = "delete from UserProfiles where Id in (select Id from AspNetUsers where UserName in ('alfred', 'moana', 'wayne'))";
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = "delete from AspNetUsers where username in ('alfred','moana','wayne')";
+                    cmd.ExecuteNonQuery();
+
+                    connection.Close();
+                }
+            }
         }
     }
 }
