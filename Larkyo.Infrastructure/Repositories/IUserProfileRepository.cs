d1e352471d05ad4352d44d0bf2c532113732d847
Kieran Tsai <kierantsai@gmail.com>
2017-09-27 22:38:15 +1300

    Added user profile respository interface to extended standard entity repository with the ability to update ignoring relationships.

---
+++ Larkyo.Infrastructure/Repositories/IUserProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Larkyo.Infrastructure.Repositories
+{
+    public interface IUserProfileRepository<TUserProfile> : IRepository<TUserProfile>
+        where TUserProfile : class
+    {
+        void UpdateWithoutRelatedEntities(TUserProfile userProfile);
+    }
+}
